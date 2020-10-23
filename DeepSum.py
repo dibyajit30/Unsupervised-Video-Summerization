536e5538a7411c15742877fa0cbb6f6e7206f62f
rohith <grohith327@gmail.com>
2020-10-23 15:23:00 -0400

    added DeepSum - train only

--- DeepSum.py
+++ DeepSum.py
@@ -2,10 +2,35 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Beroulli
+from torch.distributions import Bernoulli
 from torch.utils import data
 import json
 import logging
+from utils import AverageMeter, seed_everything, get_filename
+from reward import compute_reward
+import argparse
+from tqdm.auto import tqdm
+import glob
+
+parser = argparse.ArgumentParser(description="Video summarization through Deep RL")
+parser.add_argument(
+    "--run_name", default="", type=str, required=True, help="name to identify exp",
+)
+parser.add_argument(
+    "--epochs", default=60, type=int, help="number of epochs",
+)
+parser.add_argument(
+    "--num_episodes", default=5, type=int, help="number of episodes",
+)
+parser.add_argument(
+    "--seed", default=1, type=int, required=True, help="seed",
+)
+args = parser.parse_args()
+
+seed_everything(seed=args.seed)
+logging.basicConfig(
+    filename=f"logs/{args.run_name}.log", level=logging.INFO, format="%(message)s",
+)
 
 
 class VideoDataset(data.Dataset):
@@ -15,106 +40,16 @@ class VideoDataset(data.Dataset):
 
     def __getitem__(self, i):
         path = self.paths[i]
+        id = get_filename(path)
         feature = torch.load(path)
-        return feature
+        return feature, id
 
     def __len__(self):
         return len(self.paths)
 
 
-class RolloutBuffer:
-    def __init__(self, rollout_steps, gamma, device):
-        self.rollout_steps = rollout_steps
-        self.gamma = gamma
-        self.device = device
-        self.states = None
-        self.rewards = None
-        self.actions = None
-        self.count = None
-        self.reset()
-
-    def reset(self):
-        self.states = [None] * self.rollout_steps
-        self.rewards = [None] * self.rollout_steps
-        self.actions = [None] * self.rollout_steps
-        self.count = 0
-
-    def store(self, state, reward, action):
-        self.states[self.count] = state
-        self.rewards[self.count] = (self.gamma ** self.count) * reward
-        self.actions[self.count] = action
-        self.count += 1
-
-    def compute_returns(self):
-        returns = []
-        for i in range(self.count):
-            returns.append(sum(self.rewards[i : self.count]) / self.gamma ** i)
-        return returns
-
-    def get_values(self):
-        states = torch.stack(self.states[: self.count]).to(self.device)
-        actions = torch.tensor(self.actions[: self.count]).to(self.device).long()
-        returns = self.compute_returns()
-        returns = torch.stack(returns).to(self.device)
-        self.reset()
-        return states, returns, actions
-
-
-def compute_reward(seq, actions, ignore_far_sim=True, temp_dist_thre=20, use_gpu=False):
-    """
-    Copied from: https://github.com/KaiyangZhou/pytorch-vsumm-reinforce
-    """
-    _seq = seq.detach()
-    _actions = actions.detach()
-    pick_idxs = _actions.squeeze().nonzero().squeeze()
-    num_picks = len(pick_idxs) if pick_idxs.ndimension() > 0 else 1
-
-    if num_picks == 0:
-        # give zero reward is no frames are selected
-        reward = torch.tensor(0.0)
-        if use_gpu:
-            reward = reward.cuda()
-        return reward
-
-    _seq = _seq.squeeze()
-    n = _seq.size(0)
-
-    # compute diversity reward
-    if num_picks == 1:
-        reward_div = torch.tensor(0.0)
-        if use_gpu:
-            reward_div = reward_div.cuda()
-    else:
-        normed_seq = _seq / _seq.norm(p=2, dim=1, keepdim=True)
-        dissim_mat = 1.0 - torch.matmul(
-            normed_seq, normed_seq.t()
-        )  # dissimilarity matrix [Eq.4]
-        dissim_submat = dissim_mat[pick_idxs, :][:, pick_idxs]
-        if ignore_far_sim:
-            # ignore temporally distant similarity
-            pick_mat = pick_idxs.expand(num_picks, num_picks)
-            temp_dist_mat = torch.abs(pick_mat - pick_mat.t())
-            dissim_submat[temp_dist_mat > temp_dist_thre] = 1.0
-        reward_div = dissim_submat.sum() / (
-            num_picks * (num_picks - 1.0)
-        )  # diversity reward [Eq.3]
-
-    # compute representativeness reward
-    dist_mat = torch.pow(_seq, 2).sum(dim=1, keepdim=True).expand(n, n)
-    dist_mat = dist_mat + dist_mat.t()
-    dist_mat.addmm_(1, -2, _seq, _seq.t())
-    dist_mat = dist_mat[:, pick_idxs]
-    dist_mat = dist_mat.min(1, keepdim=True)[0]
-    reward_rep = torch.exp(-dist_mat.mean())  # representativeness reward [Eq.5]
-
-    # combine the two rewards
-    reward = (reward_div + reward_rep) * 0.5
-
-    return reward
-
-
 class PolicyNet(nn.Module):
-    def __init__(self, in_dim=2048, hid_dim=256, num_layers=2, dropout=0.2):
+    def __init__(self, in_dim=2048, hid_dim=256, num_layers=1, dropout=0.0):
         super().__init__()
         self.rnn = nn.LSTM(
             input_size=in_dim,
@@ -130,4 +65,122 @@ class PolicyNet(nn.Module):
         h, _ = self.rnn(x)
         out = torch.sigmoid(self.head(h))
         return out
+
+
+class REINFORCE:
+    def __init__(
+        self,
+        train_paths,
+        val_paths,
+        args,
+        fold,
+        gamma=0.99,
+        beta=0.01,
+        lr=0.001,
+        device="cpu",
+    ):
+        self.policy = PolicyNet()
+        self.policy.to(device)
+        self.optimizer = torch.optim.AdamW(self.policy.parameters(), lr=lr)
+
+        self.baselines = self.load_baselines(train_paths)
+
+        self.train_dataloader, self.val_dataloader = self.load_dataloader(
+            train_paths, val_paths
+        )
+
+        self.device = device
+        self.args = args
+        self.fold = fold
+        self.beta = beta
+
+    def load_dataloader(self, train_paths, val_paths):
+        train_dataset = VideoDataset(train_paths)
+        val_dataset = VideoDataset(val_paths)
+
+        train_dataloader = data.DataLoader(
+            train_dataset, batch_size=1, shuffle=True, num_workers=4, pin_memory=True
+        )
+        val_dataloader = data.DataLoader(
+            val_dataset, batch_size=1, shuffle=False, num_workers=4, pin_memory=True
+        )
+
+        return train_dataloader, val_dataloader
+
+    def load_baselines(self, paths):
+        baselines = {}
+        for path in paths:
+            id = get_filename(path)
+            baselines[id] = 0.0
+        return baselines
+
+    def save_policy(self):
+        self.policy.eval()
+        torch.save(
+            {
+                "model": self.policy.state_dict(),
+                "optimizer": self.optimizer.state_dict(),
+            },
+            f"models/{self.args.run_name}_fold{self.fold}.pth",
+        )
+        self.policy.train()
+
+    def load_policy(self, path):
+        ckpt = torch.load(path)
+        self.policy.load_state_dict(ckpt["model"])
+        self.optimizer.load_state_dict(ckpt["optimizer"])
+
+    def learn(self, epochs, num_episodes):
+        for epoch in range(epochs):
+            losses = AverageMeter()
+
+            pbar = tqdm(self.train_dataloader, desc=f"Epoch {epoch+1}/{epochs}")
+            for idx, (feature, id) in enumerate(pbar):
+                id = id[0]
+                feature = feature.to(self.device)
+                probs = self.policy(feature)
+
+                loss = -self.beta * (probs.mean() - 0.5) ** 2
+                distr = Bernoulli(probs)
+                rewards = AverageMeter()
+
+                for _ in range(num_episodes):
+                    actions = distr.sample()
+                    log_probs = distr.log_prob(actions)
+                    reward = compute_reward(
+                        feature, actions, use_gpu=True, temp_dist_thre=40
+                    )
+                    loss = loss - log_probs.mean() * (reward - self.baselines[id])
+                    rewards.update(reward.item())
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                self.baselines[id] = 0.9 * self.baselines[id] + 0.1 * rewards.avg
+
+                losses.update(loss.item())
+                pbar.set_postfix(loss=losses.avg, reward=rewards.avg)
+                logging.info(
+                    "Epoch: {}, Step: {}, Loss: {}, Reward: {}".format(
+                        epoch + 1, idx + 1, loss.item(), rewards.avg
+                    )
+                )
+
+            self.save_policy()
+
+
+for fold in range(5):
+    f = open(f"folds/fold_{fold}.json")
+    dataset = json.load(f)
+    f.close()
+    train_paths = dataset["train"]
+    val_paths = dataset["val"]
+    agent = REINFORCE(
+        train_paths=train_paths, val_paths=val_paths, args=args, fold=fold,
+    )
+    logging.info(f"Fold: {fold}")
+    print(f"Fold: {fold}")
+    agent.learn(args.epochs, args.num_episodes)
+    logging.info("--------------------------------------")
+    print("--------------------------------------")
 
