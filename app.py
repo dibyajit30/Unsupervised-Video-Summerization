2bff6e2e79247945ec14525f12c5c93461491394
dibyajit <dibyajit30@gmail.com>
2020-12-15 00:26:23 -0500

    Send summarized video back to client

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ port = 5000
 def get_results(request):
     file = request.files['video-file']
     filename = file.filename
+    print("Received file: " + filename)
     file.save(filename)
     summary_filename = 'summary.mp4'
     summarize(filename, summary_filename)
+    print("Summarized into: " + summary_filename)
     os.remove(filename)
-    return send_file(summary_filename, mimetype='video/mp4')
+    return send_file(summary_filename, mimetype='video/mp4', as_attachment=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
